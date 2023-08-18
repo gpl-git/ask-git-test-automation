053b4cc03d19ff1afb90e64e06a2fe18df2913f6
kalailara <kalailarisa@gmail.com>
2023-08-18 02:09:30 -0400

    Created Step Definitions file

---
+++ src/test/java/support/definitions/SingleChoiceOptionStepDefs.java
@@ -0,0 +1,11 @@
+package support.definitions;
+
+import io.cucumber.java.en.Given;
+
+public class SingleChoiceOptionStepDefs {
+    @Given("I open {string} page")
+    public void iOpenPage(String url) {
+
+
+    }
+}
